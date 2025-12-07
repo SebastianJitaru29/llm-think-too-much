adef6ebd6afe7c4626d7fd6193d2ba0630368a74
sjitaru29@gmail.com <sjitaru29@gmail.com>
2025-12-07 17:25:37 +0100

    chore: launched jobs

--- launch_experiments.sh
+++ launch_experiments.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #SBATCH --mem=64GB
-#SBATCH --time=12:00:00
-#SBATCH --job-name=aime_job_long
+#SBATCH --time=34:00:00
+#SBATCH --job-name=math1  
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=8
-#SBATCH --output=logs/aime_job_long.out
-#SBATCH --error=logs/aime_job_long.err
+#SBATCH --output=logs/math1.out
+#SBATCH --error=logs/math1.err
 
 echo "Job started on $(hostname) at $(date)"
 module purge
 #module load PyTorch-bundle/2.1.2-foss-2023a-CUDA-12.1.1
 source .llmvenv/bin/activate
 srun python3 launch_experiments.py \
-  --data ./data/aime.parquet \
-  --model-path ./models/L1-Qwen3-8B-Max \
+  --data ./data/math_part1.parquet \
+  --model-path ../../../scratch/s6019595/models/L1-Qwen3-8B-Max/ \
   --generated-dir ./data/generated \
-  --file-name aime_results_long
+  --file-name math1_results_long
 echo "Job ended at $(date)"
