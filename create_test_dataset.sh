e56f91726c698b274308716abe69e316b8202dbc
tovaris <viktor.veselyvv@gmail.com>
2025-12-11 23:02:57 +0100

    test math

---
+++ create_test_dataset.sh
@@ -0,0 +1,14 @@
+mkdir -p data
+
+python3 - <<'EOF'
+import pandas as pd
+from pathlib import Path
+
+# Load parquet from HF dataset and save locally
+df_500 = pd.read_json(
+        "hf://datasets/HuggingFaceH4/MATH-500/test.jsonl",
+        lines=True
+)
+df.to_parquet("./data/test_math.parquet")
+EOF
+
