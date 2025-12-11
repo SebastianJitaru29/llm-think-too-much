e84ee4ad05b09207d2538f0560a186e84af45a12
tovaris <viktor.veselyvv@gmail.com>
2025-12-12 00:59:23 +0100

    greedy batch size

--- launch_hidden_states.sh
+++ launch_hidden_states.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=0:25:00
 #SBATCH --job-name=hidden_states
 #SBATCH --partition=gpu
-#SBATCH --gpus-per-node=1
+#SBATCH --gpus-per-node=a100:1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --output=logs/hidden_states.out
@@ -14,4 +14,4 @@ module load Python/3.11.5-GCCcore-13.2.0
 module load CUDA/12.1.1
 
 source .venv/bin/activate
-srun python get_hidden_states.py --data ./data/math.parquet --model ./models/L1-Qwen3-8B-Max/ --output ./regressor/innit_hidden_states/hidden_states_math.npy --batch-size 8
+srun python get_hidden_states.py --data ./data/math.parquet --model ./models/L1-Qwen3-8B-Max/ --output ./regressor/innit_hidden_states/hidden_states_math.npy --batch-size 32
