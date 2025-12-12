45fb48c28b24d46981221e038eea13e497dc8f81
tovaris <viktor.veselyvv@gmail.com>
2025-12-12 01:23:02 +0100

    hehe

--- regressor/train.py
+++ regressor/train.py
@@ -177,7 +177,7 @@ def train(
     x_valid, y_valid = x[valid_idx], y[valid_idx]
 
     
-    layers = (1_536, 1024, 512, 256, 10)
+    layers = (4_096, 1024, 512, 256, 10)
 
     network = Regressor(
         arch=Regressor.init_mlp(
