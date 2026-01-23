abb1356c317bdc446aa5fb96dada5750fb17978a
tovaris <viktor.veselyvv@gmail.com>
2026-01-23 12:11:49 +0100

    I forgot source

--- launch_hidden_states.sh
+++ launch_hidden_states.sh
@@ -13,8 +13,7 @@
 # module load Python/3.11.5-GCCcore-13.2.0
 # module load CUDA/12.1.1
 
-# source .venv/bin/activate
-
+source .venv/bin/activate
 export CUDA_VISIBLE_DEVICES=MIG-76de4d8d-dd90-510e-bc47-b5feb5c965b3
 python get_hidden_states.py --data ./data/actual_train.parquet --model ./models/L1-Qwen3-8B-Max/ --output ./regressor/innit_hidden_states/hidden_states_train.npy --batch-size 8
 python get_hidden_states.py --data ./data/actual_test.parquet --model ./models/L1-Qwen3-8B-Max/ --output ./regressor/innit_hidden_states/hidden_states_test.npy --batch-size 8
