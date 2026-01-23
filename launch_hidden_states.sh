636687481e5ca15a1be868d87b81bfd57cb40016
tovaris <viktor.veselyvv@gmail.com>
2026-01-23 12:06:06 +0100

    hidden states new cluster

--- launch_hidden_states.sh
+++ launch_hidden_states.sh
@@ -9,11 +9,14 @@
 #SBATCH --output=logs/hidden_states.out
 #SBATCH --error=logs/hidden_states.err
 
-module purge
-module load Python/3.11.5-GCCcore-13.2.0
-module load CUDA/12.1.1
+# module purge
+# module load Python/3.11.5-GCCcore-13.2.0
+# module load CUDA/12.1.1
 
-source .venv/bin/activate
-srun python get_hidden_states.py --data ./data/math.parquet --model ./models/L1-Qwen3-8B-Max/ --output ./regressor/innit_hidden_states/hidden_states_math.npy --batch-size 6
+# source .venv/bin/activate
+
+export CUDA_VISIBLE_DEVICES=MIG-76de4d8d-dd90-510e-bc47-b5feb5c965b3
+python get_hidden_states.py --data ./data/actual_train.parquet --model ./models/L1-Qwen3-8B-Max/ --output ./regressor/innit_hidden_states/hidden_states_train.npy --batch-size 8
+python get_hidden_states.py --data ./data/actual_test.parquet --model ./models/L1-Qwen3-8B-Max/ --output ./regressor/innit_hidden_states/hidden_states_test.npy --batch-size 8
 # python get_hidden_states.py --data ./data/aime.parquet --model ./models/L1-Qwen3-8B-Max/ --output ./regressor/innit_hidden_states/hidden_states_aime.npy --batch-size 8
 # python get_hidden_states.py --data ./data/test_all.parquet --model ./models/L1-Qwen3-8B-Max/ --output ./data/hidden_states_test.npy --batch-size 8
