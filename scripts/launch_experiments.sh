4da4b7511b6f9b89eb4082cac64a88fdccad098b
sjitaru29@gmail.com <sjitaru29@gmail.com>
2026-03-08 12:26:19 +0100

    fix: script run

--- scripts/launch_experiments.sh
+++ scripts/launch_experiments.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 #SBATCH --mem=64GB
-#SBATCH --time=48:00:00
+#SBATCH --time=60:00:00
 #SBATCH --job-name=train
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:a100:1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4 
-#SBATCH --output=logs/train_%j.out
-#SBATCH --error=logs/train_%j.err
+#SBATCH --output=logs/train_long_%j.out
+#SBATCH --error=logs/train_long_%j.err
 echo "Job started on $(hostname) at $(date)"
 module purge
 #module load PyTorch-bundle/2.1.2-foss-2023a-CUDA-12.1.1
 source /home4/s6019595/.llmvenv/bin/activate
-srun python3 home4/s6019595/launch_experiments.py \
-  --data home4/s6019595/data/raw/train_data.parquet \
+cd
+srun python3 /home4/s6019595/llm-think-too-much/launch_experiments.py \
+  --data /home4/s6019595/llm-think-too-much/data/raw/train_data.parquet \
   --model-path /scratch/s6019595/models/L1-Qwen3-8B-Max/ \
-  --generated-dir ./data/processed \
+  --generated-dir /home4/s6019595/llm-think-too-much/data/processed \
   --file-name train
 echo "Job ended at $(date)"
